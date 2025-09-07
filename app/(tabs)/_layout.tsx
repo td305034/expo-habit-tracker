6686b3d74de40671faba9789b90b6dc5915f30de
td305034 <elektroman35@gmail.com>
2025-09-07 18:14:12 +0200

    project initialization
    Initialize project with two screens as Tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,22 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { Stack, Tabs } from "expo-router";
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+export default function RootLayout() {
+  return <Tabs>
+    <Tabs.Screen name="index" options={{
+      title: "Home",
+      tabBarIcon: ({ color }) => (<FontAwesome5 name="home" size={24} color={color} />)
+    }} />
+    <Tabs.Screen name="login" options={{
+      title: "Login",
+      tabBarIcon: ({ color, focused }) => {
+        return focused ?
+          (<MaterialCommunityIcons name="brightness-percent" size={24} color={color} />)
+          :
+          (<FontAwesome6 name="percent" size={24} color={color} />)
+      }
+    }} />
+  </Tabs>
 }
