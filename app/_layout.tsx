d382f51c3fc49777713a250125cd4190f0ec25ef
td305034 <elektroman35@gmail.com>
2025-09-09 11:05:19 +0200

    feat: add route guard mechanism
    setTimeout function is used for workaround with a bug

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,30 @@
-import { Stack } from "expo-router";
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider } from "@ui-kitten/components";
+import { Stack, useRouter } from "expo-router";
+import { useEffect } from "react";
 
+function RouteGuard({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const isAuth = false;
+
+  useEffect(() => {
+    if (!isAuth) {
+      setTimeout(() => {
+          router.replace("/auth/sign-in");
+        }, 1)
+    }
+  });
+
+  return <>{children}</>
+}
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-  </Stack>
+  return (
+    <ApplicationProvider {...eva} theme={eva.light} >
+        <RouteGuard>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          </Stack>
+        </RouteGuard>
+    </ApplicationProvider>
+  )
 }
